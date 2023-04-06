d2d18d5859c0984b0b27a7d84dcb249b14eace9d
Arman <arman.kumaraswamy@gmail.com>
2023-04-06 14:17:03 -0400

    start education page

---
+++ src/assets/EDUCATION_LIST.js
@@ -0,0 +1,44 @@
+export const EDUCATION_LIST = [
+    {
+        title: 'Healthcare Inequity',
+        description:
+            'Health inequities are systematic differences in health outcomes. Health inequities are differences in health status or in the distribution of health resources between different population groups, arising from the social conditions in which people are born, grow, live, work and age. When people from underprivileged or underrepresented societies face healthcare issues, there is no guarantee that they will face equal treatment. For example, one of the clearest examples is the difference between infant health and mortality among Black and white babies born in the U.S. Black people are more likely than white people to have babies with a low birth weight. This not only demonstrates the negative effects of racial equality in healthcare, but also demonstrates the detrimental impact of deep-rooted societal inequity in areas that do not have high incomes or employment rates. The inequity faced in healthcare settings leads to unequal treatment, especially based on race or income status, which results in low-income neighborhoods or communities having very little access to affordable and reliable healthcare.',
+        links: [
+            {
+                title: 'Health inequities and their causes',
+                link: 'https://www.who.int/news-room/facts-in-pictures/detail/health-inequities-and-their-causes',
+            },
+            {
+                title: 'What is health inequity? Definition, examples, and root causes',
+                link: 'https://www.medicalnewstoday.com/articles/health-inequity',
+            },
+            {
+                title: '6 Examples of Health Disparities & Potential Solutions',
+                link: 'https://healthadministrationdegree.usc.edu/blog/examples-of-health-disparities/',
+            },
+        ],
+        images: [''],
+    },
+    {
+        title: 'Housing Inequity',
+        description:
+            'Housing inequality is a disparity in the quality of housing in a society which is a form of economic inequality. The right to housing is recognized by many national constitutions, and the lack of adequate housing can have adverse consequences for an individual or a family. The benefits from homeownership have not been shared equally.  In the second quarter of 2022, the homeownership rate for white households was 75 percent compared to 45 percent for Black households, 48 percent for Hispanic households, and 57 percent for non-Hispanic households of any other race.  Like the overall racial wealth gaps, these gaps in homeownership rates have changed little over the last three decades. In fact, the Black-white gap in homeownership rates was the same in 2020 as it was in 1970, just two years after the passage of the Fair Housing Act of 1968, which sought to end racial discrimination in the housing market. The structural racial inequities built into the American housing system target those of low income, deep-rooted inequity and cause an obviously unstable crisis for those who have wealth disparities.',
+        links: [
+            {
+                title: 'Racial Differences in Economic Security: Housing',
+                link: 'https://home.treasury.gov/news/featured-stories/racial-differences-in-economic-security-housing',
+            },
+            {
+                title: 'Systemic Inequality: Displacement, Exclusion, and Segregation',
+                link: 'https://www.americanprogress.org/article/systemic-inequality-displacement-exclusion-segregation/',
+            },
+            {
+                title: 'Time for justice: Tackling race inequalities in health and housing',
+                link: 'https://www.brookings.edu/research/time-for-justice-tackling-race-inequalities-in-health-and-housing/?amp',
+            },
+        ],
+        images: [
+            'https://i0.wp.com/www.brookings.edu/wp-content/uploads/2016/10/brook-big-ideas_fig_3-1.png?w=768&crop=0%2C0px%2C100%2C9999px&ssl=1',
+        ],
+    },
+];
--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { EDUCATION_LIST } from '../assets/EDUCATION_LIST';
 
 function Education() {
-    return <div>Education</div>;
+    return (
+        <div class="w-full pb-20 bg-gray-50 px-10 flex flex-col items-center">
+            <div class="container flex justify-center mx-auto pt-16">
+                <div className="w-full">
+                    <h1 class="xl:text-4xl text-3xl text-center text-gray-800 font-extrabold pb-6 sm:w-4/6 w-5/6 mx-auto">
+                        Education
+                    </h1>
+                </div>
+            </div>
+            <div className="w-8/12 mt-8 font-serif">
+                {EDUCATION_LIST.map((section) => (
+                    <div class="flex flex-col mb-28 gap-8 justify-center items-center">
+                        <h2 className="text-xl font-semibold">
+                            {section.title}
+                        </h2>
+                        <p className="text-lg leading-8">
+                            {section.description}
+                        </p>
+                        {section.images.map((imageLink) => (
+                            <img className="w-7/12" src={imageLink} />
+                        ))}
+                        {section.links.map((link) => (
+                            <a
+                                href={link.link}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className={`text-secondary underline`}
+                            >
+                                {link.title}
+                            </a>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Education;
